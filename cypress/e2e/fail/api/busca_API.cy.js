ac82e3d2443c4ac839d78cf9401774f159f3f74d
Guilherme Blota Neves <101132141+GuilhermeBlotaNeves@users.noreply.github.com>
2024-09-20 18:19:15 -0300

    Update busca_API.cy.js

--- cypress/e2e/fail/api/busca_API.cy.js
+++ cypress/e2e/fail/api/busca_API.cy.js
@@ -2,14 +2,13 @@ import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 /*
 Infelizmente, a API parece estar enfrentando problemas, 
-o que impede a realização do teste. Como resultado, 
-também não é possível testar a atualização da imagem de um produto (PUT).
+o que impede a realização do teste.
 */
 
 Given('que eu faço uma busca pelo produto {string}', (produto) => {
   cy.request({
     method: 'GET',
-    url: `https://www.advantageonlineshopping.com/catalog/api/v1/products/search`
+    url: `https://www.advantageonlineshopping.com/catalog/api/v1/products/search?query=${produto}`
   }).as('searchResponse');
 });
 
@@ -19,6 +18,19 @@ Then('eu devo ver produtos relacionados à busca', () => {
     expect(response.body.products).to.be.an('array').that.is.not.empty;
   });
 });
+
+Then('eu devo ver pelo menos um produto que contenha {string}', (termo) => {
+  cy.get('@searchResponse').then((response) => {
+    const produtos = response.body.products;
+    const temProduto = produtos.some(product => product.productName.toLowerCase().includes(termo.toLowerCase()));
+    expect(temProduto).to.be.true;
+  });
+});
+
+Then('o status code deve ser {int}', (statusCode) => {
+  cy.get('@searchResponse').its('status').should('eq', statusCode);
+});
+
 
 Then('o status code deve ser {int}', (statusCode) => {
   cy.get('@searchResponse').then((response) => {
